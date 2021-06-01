66b920bc0533e5d1848dd9c378e7b71bd55d98dc
BISHNU PRASAD SINGH <bpsingh4040@gmail.com>
2021-06-01 15:12:00 +0530

    Create dp_54.java

---
+++ GEEKS FOR GEEKS/DYNAMIC PROGRAMMING/dp_54.java
@@ -0,0 +1,19 @@
+class A
+{
+	public static void main(String args[])
+	{
+		int R,C;
+		System.out.println("Enter the number of rows and columns: ");
+		int R=sc.nextInt();
+		int C=sc.nextInt();
+		int M[][]=new int[R][C];
+		System.out.println("Enter the elements of the Matrix: ");
+		for(int i=0; i<R; i++)
+		{
+		   for(int j=0; j<C; j++)
+		   {
+			
+		   }
+		}
+	}
+}
